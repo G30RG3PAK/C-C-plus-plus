0fdd4660faf16dbd77f3a052288fa4c4524462bb
Pakpoom Rachtrachoo <33973448+G30RG3PAK@users.noreply.github.com>
2019-02-28 18:10:29 +0700

    Simple If statement

---
+++ Simple_IF_Statement.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    printf("Enter the the interger :");
+    scanf("%d",&i);
+    if (i > 20)
+    {
+        printf("This input is more than 20!!\n");
+    }
+    printf("This program is awesome!!\n");
+    
+
+}
